ed0b3c4acb7e0a9a7780c317b69ae55dd4ccca29
ppopow <pawel.popow@gmail.com>
2020-10-19 17:10:45 +0200

    nowe zadanie36

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function sprawdzLiczbe(liczba1, liczba2) {
+let result = liczba1 * liczba2;
+if ((liczba1 == 20) || (liczba2 == 20) || (liczba1 + liczba2 <= 20)) {  
+    return true;
+}   
+}
+
+
+
